80d918ea179c1ebc1ff3e33f712ce47ca884edb6
kimsangjunv1 <ingyoe64@gmail.com>
2022-10-07 02:21:37 +0900

    2022.10.07
    add(1)

--- board/boardSearch.php
+++ board/boardSearch.php
@@ -2,7 +2,6 @@
     include "../connect/connect.php";
     include "../connect/session.php";
 ?>
-
 <!DOCTYPE html>
 <html lang="ko">
 <head>
@@ -10,7 +9,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>게시판</title>
-
     <?php include "../include/link.php" ?>
 </head>
 <body>
@@ -20,84 +18,77 @@
         <a href="#footer">푸터 영역 바로가기</a>
     </div>
     <!-- //skip -->
-
     <?php include "../include/header.php" ?>
     <!-- //header -->
-
     <main id="main">
         <section id="board" class="container">
             <h2>게시판 영역입니다.</h2>
             <div class="board__inner">
                 <div class="board__title">
-                    <h3>검색결과</h3>
+                    <h3>검색 결과 게시판</h3>
 <?php
-    function msg($alert){
-        echo "<p>총 ".$alert."건이 검색되었습니다.</p>";
-    }
-
     if(isset($_GET['page'])){
         $page = (int) $_GET['page'];
     } else {
         $page = 1;
     }
 
-    $viewNum = 10;
-    $viewLimit = ($viewNum * $page) - $viewNum;
-
-    echo $_GET['page'];
-
+    function msg($alert){
+        echo "<p>총 ".$alert."건이 검색되었습니다.</p>";
+    }
     $searchKeyword = $_GET['searchKeyword'];
     $searchOption = $_GET['searchOption'];
-    // echo $searchKeyword,$searchOption
-
+    
     $searchKeyword = $connect -> real_escape_string(trim($searchKeyword));
     $searchOption = $connect -> real_escape_string(trim($searchOption));
-    
-    //쿼리문(JOIN)
-    //b.myBoardID, b.boardTitle, b.boaedContents, m.youName, b.regTime, b.boardView
-    // $sql = "SELECT b.myBoardID, b.boardTitle, b.boardContents, m.youName, b.regTime, b.boardView FROM myBoard b JOIN myMember m ON(b.myMemberID = m.myMemberID) WHERE b.boardTitle LIKE '%{$searchKeyword}%' ORDER BY myBoardID DESC LIMIT 10";
-    // $sql = "SELECT b.myBoardID, b.boardTitle, b.boardContents, m.youName, b.regTime, b.boardView FROM myBoard b JOIN myMember m ON(b.myMemberID = m.myMemberID) WHERE b.boardContents LIKE '%{$searchKeyword}%' ORDER BY myBoardID DESC LIMIT 10";
-    // $sql = "SELECT b.myBoardID, b.boardTitle, b.boardContents, m.youName, b.regTime, b.boardView FROM myBoard b JOIN myMember m ON(b.myMemberID = m.myMemberID) WHERE b.youName LIKE '%{$searchKeyword}%' ORDER BY myBoardID DESC LIMIT 10";
-    $sql = "SELECT b.myBoardID, b.boardTitle, b.boardContents, m.youName, b.regTime, b.boardView FROM myBoard b JOIN myMember m ON(b.myMemberID = m.myMemberID) ";
+
+    $sql = "SELECT b.myBoardID, b.boardTitle, b.boardContents, m.youName, b.regTime, b.boardView FROM myBoard b JOIN myMember m ON(b.myMemberID = m.myMemberID)";
+
     switch($searchOption){
-        case 'title';
-            $sql .= "WHERE b.boardTitle LIKE '%{$searchKeyword}%' ORDER BY myBoardID DESC LIMIT 10";
+        case "title":
+            $sql .= "WHERE b.boardTitle LIKE '%{$searchKeyword}%' ORDER BY myBoardID DESC ";
             break;
-        case 'content';
-            $sql .= "WHERE b.boardContents LIKE '%{$searchKeyword}%' ORDER BY myBoardID DESC LIMIT 10";
+        case "content":
+            $sql .= "WHERE b.boardContents LIKE '%{$searchKeyword}%' ORDER BY myBoardID DESC ";
             break;
-        case 'name';
-            $sql .= "WHERE m.youName LIKE '%{$searchKeyword}%' ORDER BY myBoardID DESC LIMIT 10";
+        case "name":
+            $sql .= "WHERE m.youName LIKE '%{$searchKeyword}%' ORDER BY myBoardID DESC ";
             break;
     }
     $result = $connect -> query($sql);
-    if($result){
-        $count = $result -> num_rows;
-        msg($count);
-    }
+
+    // 전체 게시글 갯수
+    $totalCount = $result -> num_rows;
+    msg($totalCount);
+
 ?>
                 </div>
-            </div>
-            <div class="board__table">
-                <table>
-                    <colgroup>
-                        <col style="width: 5%">
-                        <col>
-                        <col style="width: 10%">
-                        <col style="width: 10%">
-                        <col style="width: 7%">
-                    </colgroup>
-                    <thead>
-                        <tr>
-                            <th>번호</th>
-                            <th>제목</th>
-                            <th>등록자</th>
-                            <th>등록일</th>
-                            <th>조회수</th>
-                        </tr>
-                    </thead>
-                    <tbody>
+                <div class="board__table">
+                    <table>
+                        <colgroup>
+                            <col style="width: 5%">
+                            <col>
+                            <col style="width: 10%">
+                            <col style="width: 10%">
+                            <col style="width: 7%">
+                        </colgroup>
+                        <thead>
+                            <tr>
+                                <th>번호</th>
+                                <th>제목</th>
+                                <th>등록자</th>
+                                <th>등록일</th>
+                                <th>조회수</th>
+                            </tr>
+                        </thead>
+                        <tbody>
 <?php
+    $viewNum = 10;
+    $viewLimit = ($viewNum * $page) - $viewNum;
+
+    $sql = $sql."LIMIT {$viewLimit}, {$viewNum}";
+    $result = $connect -> query($sql);
+
     if($result){
         $count = $result -> num_rows;
 
@@ -117,34 +108,17 @@
         }
     }
 ?>
-                    </tbody>
-                </table>
-            </div>
-            <div class="board__pages">
+                        </tbody>
+                    </table>
+                </div>
+                <div class="board__pages">
                     <ul>
 <?php
-    $sql = "SELECT count(myBoardID) FROM myBoard b JOIN myMember m ON(b.myMemberID = m.myMemberID)";
-    switch($searchOption){
-        case 'title';
-            $sql .= "WHERE b.boardTitle LIKE '%{$searchKeyword}%' ORDER BY myBoardID DESC";
-            break;
-        case 'content';
-            $sql .= "WHERE b.boardContents LIKE '%{$searchKeyword}%' ORDER BY myBoardID DESC";
-            break;
-        case 'name';
-            $sql .= "WHERE m.youName LIKE '%{$searchKeyword}%' ORDER BY myBoardID DESC";
-            break;
-    }
-    $result = $connect -> query($sql);
-
-    $boardCount = $result -> fetch_array(MYSQLI_ASSOC);
-    $boardCount = $boardCount['count(myBoardID)'];
-
+    // echo $totalCount;
 
     //총 페이지 갯수;
-    $boardCount = ceil($boardCount/$viewNum);
+    $boardCount = ceil($totalCount/$viewNum);
 
-    // echo $boardCount;
 
     //현재 페이지를 기준으로 보여주고 싶은 갯수
     $pageCurrent = 5;
@@ -160,30 +134,30 @@
     // 이전 페이지, 초기 페이지
     if($page != 1){
         $prevPage = $page - 1;
-        echo "<li><a href='board.php?searchKeyword={$searchKeyword}&searchOption={$searchOption}&page=1'>처음으로</a></li>";
-        echo "<li><a href='board.php?searchKeyword={$searchKeyword}&searchOption={$searchOption}&page={$prevPage}'>이전</a></li>";
+        echo "<li><a href='boardSearch.php?page=1&searchKeyword={$searchKeyword}&searchOption={$searchOption}'>처음으로</a></li>";
+        echo "<li><a href='boardSearch.php?page={$prevPage}&searchKeyword={$searchKeyword}&searchOption={$searchOption}'>이전</a></li>";
     }
 
     // 페이지 넘버 순서
     for($i=$startPage; $i<=$endPage; $i++){
         $active = "";
         if($i == $page) $active = "active";
-        echo "<li class='{$active}'><a href='board.php?searchKeyword={$searchKeyword}&searchOption={$searchOption}&page={$i}'>{$i}</a></li>";
+        echo "<li class='{$active}'><a href='boardSearch.php?page={$i}&searchKeyword={$searchKeyword}&searchOption={$searchOption}'>{$i}</a></li>";
     }
 
     // 다음 페이지, 마지막 페이지
     if($page != $boardCount){
         $nextPage = $page + 1;
-        echo "<li><a href='board.php?searchKeyword={$searchKeyword}&searchOption={$searchOption}&page={$nextPage}'>다음</a></li>";
-        echo "<li><a href='board.php?searchKeyword={$searchKeyword}&searchOption={$searchOption}&page={$boardCount}'>마지막으로</a></li>";
+        echo "<li><a href='boardSearch.php?page={$nextPage}&searchKeyword={$searchKeyword}&searchOption={$searchOption}'>다음</a></li>";
+        echo "<li><a href='boardSearch.php?page={$boardCount}&searchKeyword={$searchKeyword}&searchOption={$searchOption}'>마지막으로</a></li>";
     }
 ?>
                     </ul>
                 </div>
+            </div>
         </section>
     </main>
     <!-- //main -->
-
     <?php include "../include/footer.php" ?>
     <!-- //footer -->
 </body>
